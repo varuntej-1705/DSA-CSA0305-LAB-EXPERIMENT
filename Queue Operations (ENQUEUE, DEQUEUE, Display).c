d0b6c214d4df694f480c1262917a7afd83417370
Varun tej <192424175.simats@saveetha.com>
2025-08-30 14:06:52 +0530

    Create Queue Operations (ENQUEUE, DEQUEUE, Display).c

---
+++ Queue Operations (ENQUEUE, DEQUEUE, Display).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define SIZE 5
+
+int q[SIZE], front=-1, rear=-1;
+
+void enqueue(int x){
+    if(rear==SIZE-1) printf("Queue Full\n");
+    else{
+        if(front==-1) front=0;
+        q[++rear]=x;
+    }
+}
+void dequeue(){
+    if(front==-1 || front>rear) printf("Queue Empty\n");
+    else printf("Dequeued: %d\n",q[front++]);
+}
+void display(){
+    if(front==-1 || front>rear) printf("Queue Empty\n");
+    else{
+        printf("Queue: ");
+        for(int i=front;i<=rear;i++) printf("%d ",q[i]);
+        printf("\n");
+    }
+}
+
+int main(){
+    enqueue(10); enqueue(20); enqueue(30);
+    display(); dequeue(); display();
+}
